08afc1ff971a39e50d817bac2d84b491b3e41270
Demetrius Magela da Mata <63015481+demetriusengdados@users.noreply.github.com>
2023-02-14 11:34:24 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,15 +1,3 @@
-from dbt.adapters.databricks.connections import DatabricksConnectionManager  # noqa
-from dbt.adapters.databricks.connections import DatabricksCredentials
-from dbt.adapters.databricks.relation import DatabricksRelation  # noqa
-from dbt.adapters.databricks.column import DatabricksColumn  # noqa
-from dbt.adapters.databricks.impl import DatabricksAdapter
+import os
 
-from dbt.adapters.base import AdapterPlugin
-from dbt.include import databricks
-
-Plugin = AdapterPlugin(
-    adapter=DatabricksAdapter,  # type: ignore[arg-type]
-    credentials=DatabricksCredentials,
-    include_path=databricks.PACKAGE_PATH,
-    dependencies=["spark"],
-)
+PACKAGE_PATH = os.path.dirname(__file__)
